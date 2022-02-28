b70a681cc2218299f397f808a503ef764ca0a569
IAmToufiqurRahman <toufiqurrahmanaust@gmail.com>
2022-03-01 00:20:16 +0600

    connected to database & registered users

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// Connect to database
+connectDB()
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' })
