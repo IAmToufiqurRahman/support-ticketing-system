b4034ce90440106be7e1b676116ad56cab2890e9
IAmToufiqurRahman <toufiqurrahmanaust@gmail.com>
2022-03-11 16:06:10 +0600

    ticket model & routes, new ticket form

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -7,6 +7,6 @@ const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
-router.route('/:id').get(protect, getTicket).get(protect, deleteTicket).put(protect, updateTicket)
+router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket)
 
 module.exports = router
